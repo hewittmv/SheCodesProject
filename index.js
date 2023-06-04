e08749e66337f2aed4e6937c64b60949dc73277f
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-06-04 21:07:22 +0100

    Add celsius /Fahrenheit conversion & styling

--- index.js
+++ index.js
@@ -39,19 +39,28 @@ function formatDate(date) {
 }
 
 function showCityTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
   let minTemp = Math.round(response.data.main.temp_min);
   let maxTemp = Math.round(response.data.main.temp_max);
+  let windSpeed = Math.round(response.data.wind.speed);
   let minTempElement = document.querySelector("#minimum-temperature");
-  let temperatureElement = document.querySelector("#city-Temp");
+  let temperatureElement = document.querySelector("#celsius-temp");
   let maxTempElement = document.querySelector("#maximum-temperature");
-  let weatherDescription = document.querySelector("h4");
-  let cityName = document.querySelector("h2");
-  temperatureElement.innerHTML = `${temperature}°C`;
+  let weatherDescription = document.querySelector("#weather-description");
+  let cityName = document.querySelector("#city-text");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   minTempElement.innerHTML = `${minTemp}°C`;
   maxTempElement.innerHTML = `${maxTemp}°C`;
   weatherDescription.innerHTML = response.data.weather[0].description;
   cityName.innerHTML = response.data.name;
+  windSpeedElement.innerHTML = `${windSpeed}`;
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  celsiusTemperature = response.data.main.temp;
 }
 
 function searchCity(city) {
@@ -71,14 +80,32 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showCityTemperature);
 }
+
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
+}
+function displayFahrenheit(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#celsius-temp");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+function displayCelsius(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#celsius-temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 let dateElement = document.querySelector("h6");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
+
+let celsiusTemperature = null;
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
@@ -86,4 +113,10 @@ searchForm.addEventListener("submit", handleSubmit);
 let myLocationButton = document.querySelector("#myLocation");
 myLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("Tokyo");
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", "displayFahrenheit");
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsius);
+
+searchCity("New York");
