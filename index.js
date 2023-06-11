f4122a0f6ee5ae7b0d996f0fbdcb7e43d5c24f38
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-06-11 13:33:34 +0100

    Remove weather forecast fake data HTML

--- index.js
+++ index.js
@@ -82,9 +82,9 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
-function getForecast(coords) {
+function getForecast(coordinates) {
   let apiKey = "6f578b96aa9505bcce148ac22cb85794";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 function showCityTemperature(response) {
