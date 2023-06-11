3108a7d31cbe29d95be62a186ac2d5a8a020f414
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-06-11 19:39:32 +0100

    API url version change
    API URL version change from 3 to 2.5

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 function showCityTemperature(response) {
