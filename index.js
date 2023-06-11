5b81cdb60d5f0f7d8b7861aae70c19e362c113c2
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-06-11 12:31:14 +0100

    add units to apiUrl in get coordinates function

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function displayForecast(response) {
 
 function getForecast(coords) {
   let apiKey = "6f578b96aa9505bcce148ac22cb85794";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 function showCityTemperature(response) {
